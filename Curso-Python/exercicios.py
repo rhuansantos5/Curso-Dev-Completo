b105a4e25e8d294a3bc7f7b0001783b0f7668e19
rhuansantos5 <ladariorhuan@gmail.com>
2026-04-18 15:30:30 -0300

    Removi os comentarios de dos exercicios

--- Curso-Python/exercicios.py
+++ Curso-Python/exercicios.py
@@ -1,60 +1,60 @@
 # # Exercicio 1
-# nome = input("Digite seu nome: ")
-# print(f"Olá, {nome}! Seja bem vindo")
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}! Seja bem vindo")
 
-# # Exercicio 2
-# num1 = int(input("Digite o um numero inteiro: "))
-# num2 = int(input("Digite o primeiro outro numero inteiro: "))
+# Exercicio 2
+num1 = int(input("Digite o um numero inteiro: "))
+num2 = int(input("Digite o primeiro outro numero inteiro: "))
 
-# resultado = num1 + num2
+resultado = num1 + num2
 
-# print(f"O resultado é {resultado}")
+print(f"O resultado é {resultado}")
 
 # Exercicio 3
 
-# nota1 = float(input("Digite a primeria nota: ")) 
-# nota2 = float(input("Digite a segunda nota: "))
+nota1 = float(input("Digite a primeria nota: ")) 
+nota2 = float(input("Digite a segunda nota: "))
 
-# media = (nota1 + nota2) / 2
-# media_round = round(media, 1)
+media = (nota1 + nota2) / 2
+media_round = round(media, 1)
 
-# print(f"A media das notas é: {media_round}")
+print(f"A media das notas é: {media_round}")
 
 # Exercicio 4
 
-# nome = input("Infome seu nome: ")
-# idade = int(input("Infome sua idade: "))
-# estado = input("Infome seu estado: ")
+nome = input("Infome seu nome: ")
+idade = int(input("Infome sua idade: "))
+estado = input("Infome seu estado: ")
 
-# print(f"{nome} tem {idade} anos e mora no estado {estado}")
+print(f"{nome} tem {idade} anos e mora no estado {estado}")
 
 # Exercicio Estrutura condicionais 
 
 # Exercicio 1
 
-# temp = float(input("Digite a temperatura em Celsius: "))
+temp = float(input("Digite a temperatura em Celsius: "))
 
-# if temp >= 30: 
-#     print("Está muito quente!")
-# elif temp >= 20 and temp < 30:
-#     print("Está agradável!")
-# else:
-#     print("Está muito frio!")    
+if temp >= 30: 
+    print("Está muito quente!")
+elif temp >= 20 and temp < 30:
+    print("Está agradável!")
+else:
+    print("Está muito frio!")    
 
 # Exercicio 2
 
-# age = int(input("Infome a idade: "))
+age = int(input("Infome a idade: "))
 
-# if age < 6:
-#     tarifa = 0.00
-# elif age >=6 and age < 18:
-#     tarifa = 5.00
-# elif age >= 18 and age < 60: 
-#     tarifa = 10.00
-# else:
-#     tarifa = 0.00
+if age < 6:
+    tarifa = 0.00
+elif age >=6 and age < 18:
+    tarifa = 5.00
+elif age >= 18 and age < 60: 
+    tarifa = 10.00
+else:
+    tarifa = 0.00
 
-# print(f"O valor da tarifa: {tarifa}")
+print(f"O valor da tarifa: {tarifa}")
 
 # Exercicio 3 
 
@@ -66,13 +66,16 @@ if username == "admin" and senha == "python2025":
 else:
     print("Credenciais estão incorretas.")
 
-# username_correto = "admin" 
-# senha_correta = "python2025"
+username_correto = "admin" 
+senha_correta = "python2025"
 
-# username = input("username:")
-# senha = input("senha: ")
 
-# if (username == username_correto) and (senha == senha_correta):
-#     print("Login bem sucedido!")
-# else:
-#     print("Credenciais estão incorretas.")
+# Exercicio 3 outra maneira de fazer
+
+username = input("username:")
+senha = input("senha: ")
+
+if (username == username_correto) and (senha == senha_correta):
+    print("Login bem sucedido!")
+else:
+    print("Credenciais estão incorretas.")
