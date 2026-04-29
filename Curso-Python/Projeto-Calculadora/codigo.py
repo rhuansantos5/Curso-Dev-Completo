96ab4a456aec67561f5050d22c8edbd859aba9da
rhuansantos5 <ladariorhuan@gmail.com>
2026-04-29 18:43:49 -0300

    Criei uma nova pasta do projeto calculadora e adicionei as primeiras funções

---
+++ Curso-Python/Projeto-Calculadora/codigo.py
@@ -0,0 +1,20 @@
+def soma(a, b):
+    return a + b 
+
+def sobtracao(a, b):
+    return a - b 
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b 
+
+
+valor_1 = 10
+valor_2 = 20
+
+print(f"valor 1 + valor 2: {soma(valor_1, valor_2)}")
+print(f"valor 1 - valor 2: {sobtracao(valor_1, valor_2)}")
+print(f"valor 1 * valor 2: {multiplicacao(valor_1, valor_2)}")
+print(f"valor 1 / valor 2: {divisao(valor_1, valor_2)}")
