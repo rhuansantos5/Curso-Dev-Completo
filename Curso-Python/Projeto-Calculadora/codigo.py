fa102aebe2bd82a544beedc1a1cff3b1d9abfe12
rhuansantos5 <ladariorhuan@gmail.com>
2026-06-02 19:47:33 -0300

    Adicionei algumas funções da biblioteca math para fazer operacoes Log2 e RaizQuadrada

--- Curso-Python/Projeto-Calculadora/codigo.py
+++ Curso-Python/Projeto-Calculadora/codigo.py
@@ -1,3 +1,5 @@
+from math import sqrt, log2
+
 def soma(a, b):
     return a + b 
 
@@ -10,12 +12,21 @@ def multiplicacao(a, b):
 def divisao(a, b):
     return a / b 
 
+def raizQuadrada(a):
+    return sqrt(a)
+
+def logaritmo(a):
+    return log2(a)
+
+
 def exibir_menu():
     print("\n=== CAlCULADORA ===")
     print("1 - Soma")
     print("2 - Subtração")
     print("3 - Multiplicação")
     print("4 - Divisão")
+    print("5 - Raiz Quadrada (do resultado atual)")
+    print("6 - Logaritmo base 2 (do resultado atual)")
     print("0 - Sair\n")
 
 def formatar_resultado(resultado):
@@ -25,7 +36,7 @@ def formatar_resultado(resultado):
     return resultado
 
 def main():
-    opcoes_validas = {"1", "2", "3", "4", "0"}
+    opcoes_validas = {"1", "2", "3", "4", "5", "6", "0"}
 
     try:
         resultado_atual = float(input("Informe o valor inicial: "))
@@ -48,11 +59,12 @@ def main():
             print("Opção inválida.\n")
             continue
 
-        try:
-            valor_operando = float(input("Digite o próximo valor: ")) 
-        except ValueError:
-            print("Número inválido")
-            continue
+        if opcao_escolhida in {"1", "2", "3", "4"}:
+            try:
+                valor_operando = float(input("Digite o próximo valor: ")) 
+            except ValueError:
+                print("Número inválido")
+                continue
 
         if opcao_escolhida == "1":
             resultado_atual = soma(resultado_atual, valor_operando)
@@ -65,6 +77,14 @@ def main():
                 resultado_atual = divisao(resultado_atual, valor_operando)
             except ZeroDivisionError:
                 print("Não se pode dividir por 0")
-    print("Encerrando a calculadora")
-
+        elif opcao_escolhida == "5":
+            try:
+                resultado_atual = raizQuadrada(resultado_atual)
+            except ValueError:
+                print("Não é possivel calcular raiz quadrada de um numero negativo.")    
+        elif opcao_escolhida == "6":
+            try:
+                resultado_atual = logaritmo(resultado_atual)    
+            except ValueError:
+                print("Logaritmo só é definido para número positivo e diferente de zero.")
 main()
